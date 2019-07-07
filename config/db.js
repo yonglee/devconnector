75b64dddc874c373c86064461ec30f0092d9eda2
Yong Lee <yongslee79@hotmail.com>
2019-07-07 02:32:11 -0400

    connected mongodb atlas using mongodb

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const db = process.env.mongoDB;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
