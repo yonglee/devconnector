d93be0e3072f76ef2aff80ae9b9da1f72fbaedda
Yong Lee <yongslee79@hotmail.com>
2019-07-07 23:11:45 -0400

    added create and update user's profile

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = process.env.mongoDB;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
     console.log('MongoDB Connected...');
   } catch (err) {
