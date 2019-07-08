270f460508fa3f494afabd04f073151591716d74
Yong Lee <yongslee79@hotmail.com>
2019-07-08 11:04:48 -0400

    modified router.get('/') for /routes/api/posts to get posts from the latest posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -51,7 +51,7 @@ router.post(
 // @access  Private
 router.get('/', auth, async (req, res) => {
   try {
-    const posts = await Post.find().sort({ date: 1 });
+    const posts = await Post.find().sort({ date: -1 });
     res.json(posts);
   } catch (err) {
     console.error(err.message);
