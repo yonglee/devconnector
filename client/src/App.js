4ff59f1bf327afa96ad7cc15eee853c8010766ff
Yong Lee <yongslee79@hotmail.com>
2019-07-09 04:59:27 -0400

    added navbar & landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
 
 const App = () => (
   <>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </>
 );
 
