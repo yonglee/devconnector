ddc61b3b25383da9960fe37227b7ee77362a23b8
Yong Lee <yongslee79@hotmail.com>
2019-07-12 16:22:40 -0400

    added profile component to view the user's profile & display edit profile link if the user is logged in

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../store/actions/profileAction';
+
+const Profile = ({
+  getProfileById,
+  profile: { profile, loading },
+  auth,
+  match
+}) => {
+  useEffect(() => {
+    getProfileById(match.params.id);
+  }, [getProfileById]);
+  return (
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            !auth.loading &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </>
+      )}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profileReducer,
+  auth: state.authReducer
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfileById }
+)(Profile);
