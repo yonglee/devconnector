a5343368052aefabca8ef722d6866712144749c7
Yong Lee <yongslee79@hotmail.com>
2019-07-12 16:06:10 -0400

    fixed console warnings

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -48,7 +48,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? '' : profile.social.youtube,
       instagram: loading || !profile.social ? '' : profile.social.instagram
     });
-  }, [loading]);
+  }, [loading, getCurrentProfile]);
 
   const {
     company,
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -8,7 +8,7 @@ import { getProfiles } from '../../store/actions/profileAction';
 const Profiles = ({ getProfiles, profiles: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
-  }, []);
+  }, [getProfiles]);
   return (
     <>
       {loading ? (
