d7650c312c3bb44d90bb0d4fa5378ca9fa195247
Yong Lee <yongslee79@hotmail.com>
2019-07-13 15:49:39 -0400

    modified store.js to disable redux-devtools-extension in production

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -7,16 +7,25 @@ const initialState = {};
 
 const middleware = [thunk];
 
+// const enhancers =
+//   typeof window !== undefined && process.env.NODE_ENV !== 'production'
+//     ? composeWithDevTools(applyMiddleware(...middleware))
+//     : compose(applyMiddleware(...middleware));
+
 const enhancers =
-  typeof window !== undefined && process.env.NODE_ENV !== 'production'
-    ? composeWithDevTools(applyMiddleware(...middleware))
-    : compose(applyMiddleware(...middleware));
+  typeof window !== 'undefined' && process.env.NODE_ENV !== 'production'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f;
 
 const store = createStore(
   rootReducer,
   initialState,
-  enhancers
   // composeWithDevTools(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    enhancers
+  )
 );
 
 export default store;
